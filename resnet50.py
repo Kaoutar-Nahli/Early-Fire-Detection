9f298b07fc08167637642087ad74d643c77816b2
unknown <kautarnahli@hotmail.es>
2021-08-30 01:29:51 +0200

    OOP-class Resnet

--- resnet50.py
+++ resnet50.py
@@ -1,4 +1,7 @@
 #%%
+# from google.colab import drive
+# drive.mount('\content\drive')
+
 import torch
 import torch.nn as nn
 from torchvision import datasets, transforms, models
@@ -17,119 +20,122 @@ from sklearn import preprocessing
 from sklearn import preprocessing
 from sklearn.metrics import f1_score
 import torch.optim as optim
+import datetime
+import pandas as pd
 #%%
-transforms_train = transforms.Compose([transforms.Resize(225),
-                                       transforms.CenterCrop(224),
-                                       transforms.ToTensor(),
-                                       transforms.Normalize([0.5, 0.5, 0.5],
-                                                            [0.5, 0.5, 0.5])])
+# model from How to Train an Image Classifier in PyTorch and use it to Perform Basic Inference on Single Images
+#freezing trained layers
 
-transforms_test = transforms.Compose([transforms.Resize(225),
-                                       transforms.CenterCrop(224),
-                                       transforms.ToTensor(),
-                                       transforms.Normalize([0.5, 0.5, 0.5],
-                                                            [0.5, 0.5, 0.5])])
+class Resnet_model():
+    def __init__(self, epochs, drive=True):
+        #initializing model
+        self.model = models.resnet50(pretrained=True)
+        for param in self.model.parameters():
+            param.requires_grad = False
 
-train_data = datasets.ImageFolder(root='data\FIRE-SMOKE-DATASET\Train', transform=transforms_train)
-test_data = datasets.ImageFolder(root='data\FIRE-SMOKE-DATASET\Test', transform=transforms_test)
-print(len(train_data), len(test_data))
+        self.model.fc = nn.Sequential(nn.Linear(2048, 512),
+                                nn.ReLU(),
+                                nn.Dropout(0.2),
+                                nn.Linear(512, 10),
+                                nn.LogSoftmax(dim=1))
+        self.epochs = epochs
+        if drive:
+             self.model_name = 'model_resnet50_'+ datetime.datetime.now().strftime("%Y%m%d%H")
 
-#%%
-#spliting the data
-train_data, val_data = torch.utils.data.random_split(train_data, [2400,300])
-#%%
-train_data_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
-val_data_loader = torch.utils.data.DataLoader(val_data, batch_size=64, shuffle=True)
-test_data_loader = torch.utils.data.DataLoader(test_data,  batch_size=64, shuffle=True)
-
-
-#%%
-device = torch.device("cuda" if torch.cuda.is_available() 
-                                  else "cpu")
-model = models.resnet50(pretrained=True)
-print(model)
-#%%
-#How to Train an Image Classifier in PyTorch and use it to Perform Basic Inference on Single Images
-for param in model.parameters():
-    param.requires_grad = False
     
-model.fc = nn.Sequential(nn.Linear(2048, 512),
-                                 nn.ReLU(),
-                                 nn.Dropout(0.2),
-                                 nn.Linear(512, 10),
-                                 nn.LogSoftmax(dim=1))
-criterion = nn.NLLLoss()
-optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
-model.to(device)
+    def train(self, device, lr, train_data_loader, test_data_loader ):
+        criterion = nn.NLLLoss()
+        optimizer = optim.Adam(self.model.fc.parameters(), lr=lr)
+        self.model.to(device)
 
-#%%
-epochs = 20
-steps = 0
-running_loss = 0
-print_every = 1
-train_losses, test_losses = [], []
-for epoch in range(epochs):
-    for inputs, labels in train_data_loader:
-        steps += 1
-        inputs, labels = inputs.to(device), labels.to(device)
-        optimizer.zero_grad()
-        logps = model.forward(inputs)
-        loss = criterion(logps, labels)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-        
-        if steps % print_every == 0:
-            test_loss = 0
-            accuracy = 0
-            model.eval()
-            with torch.no_grad():
-                for inputs, labels in test_data_loader:
-                    inputs, labels = inputs.to(device),labels.to(device)
-                    logps = model.forward(inputs)
-                    batch_loss = criterion(logps, labels)
-                    test_loss += batch_loss.item()
-                    
-                    ps = torch.exp(logps)
-                    top_p, top_class = ps.topk(1, dim=1)
-                    equals = top_class == labels.view(*top_class.shape)
-                    accuracy +=torch.mean(equals.type(torch.FloatTensor)).item()
-            train_losses.append(running_loss/len(train_data_loader))
-            test_losses.append(test_loss/len(test_data_loader))                    
-            print(f"Epoch {epoch+1}/{epochs}.. "
-                  f"Train loss: {running_loss/print_every:.3f}.. "
-                  f"Test loss: {test_loss/len(test_data_loader):.3f}.. "
-                  f"Test accuracy: {accuracy/len(test_data_loader):.3f}")
-            running_loss = 0
-            model.train()
+        epochs = self.epochs
+        steps = 0
+        running_loss = 0
+        print_every = 1
+        train_losses, test_losses, accuracy_test = [], [], []
+        for epoch in range(epochs):
+            for inputs, labels in train_data_loader:
+                steps += 1
+                inputs, labels = inputs.to(device), labels.to(device)
+                optimizer.zero_grad()
+                logps = self.model.forward(inputs)
+                loss = criterion(logps, labels)
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item()
+                
+                if steps % print_every == 0:
+                    test_loss = 0
+                    accuracy = 0
+                    self.model.eval()
+                    with torch.no_grad():
+                        for inputs, labels in test_data_loader:
+                            inputs, labels = inputs.to(device),labels.to(device)
+                            logps = self.model.forward(inputs)
+                            batch_loss = criterion(logps, labels)
+                            test_loss += batch_loss.item()
+                            
+                            ps = torch.exp(logps)
+                            top_p, top_class = ps.topk(1, dim=1)
+                            equals = top_class == labels.view(*top_class.shape)
+                            accuracy +=torch.mean(equals.type(torch.FloatTensor)).item()
+                    train_losses.append(running_loss/print_every)
+                    test_losses.append(test_loss/len(test_data_loader))    
+                    accuracy_test.append(accuracy/len(test_data_loader))                
+                    print(f"Epoch {epoch+1}/{epochs}.. "
+                        f"Train loss: {running_loss/print_every:.3f}.. "
+                        f"Test loss: {test_loss/len(test_data_loader):.3f}.. "
+                        f"Test accuracy: {accuracy/len(test_data_loader):.3f}")
+                    running_loss = 0
+                    self.model.train()
+                plt.plot(train_losses, label='Training loss')
+                plt.plot(test_losses, label='Validation loss')
+                plt.show()
+                plt.savefig('./drive/MyDrive/models/Resnet_Loss_Graph_5_epochs.jpg')
+                df = pd.DataFrame(list(zip(train_losses, test_losses, accuracy_test)),
+                columns =['train_losses', 'test_losses','accuracy_test'])
+                df.to_csv(str(self.model_name)+'.csv')
 
-torch.save(model.state_dict(), 'model.pt')
+    
 
-torch.save(model, 'model.pt')
-'''
-Epoch 1/20.. Train loss: 0.220.. Test loss: 0.247.. Test accuracy: 0.899
-Epoch 1/20.. Train loss: 0.219.. Test loss: 0.215.. Test accuracy: 0.924
-Epoch 1/20.. Train loss: 0.205.. Test loss: 0.213.. Test accuracy: 0.929
-
-
-Epoch 2/20.. Train loss: 0.252.. Test loss: 0.258.. Test accuracy: 0.921
-Epoch 2/20.. Train loss: 0.343.. Test loss: 0.265.. Test accuracy: 0.919
-Epoch 2/20.. Train loss: 0.181.. Test loss: 0.257.. Test accuracy: 0.919
-Epoch 2/20.. Train loss: 0.325.. Test loss: 0.250.. Test accuracy: 0.914
-Epoch 2/20.. Train loss: 0.269.. Test loss: 0.248.. Test accuracy: 0.922
-'''
+    def save(self):
+        torch.save(self.model.state_dict(),str(self.model_name)+'.pt')
 
 
+ 
+# %%
+if __name__ =='main':
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
 
 
 
+    transforms_train = transforms.Compose([transforms.Resize(225),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize([0.5, 0.5, 0.5],
+                                                            [0.5, 0.5, 0.5])])
+
+    transforms_test = transforms.Compose([transforms.Resize(225),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize([0.5, 0.5, 0.5],
+                                                            [0.5, 0.5, 0.5])])
+
+    train_data = datasets.ImageFolder(root='data\FIRE-SMOKE-DATASET\Train', transform=transforms_train)
+    test_data = datasets.ImageFolder(root='data\FIRE-SMOKE-DATASET\Test', transform=transforms_test)#drive/MyDrive/
+    print(len(train_data), len(test_data))
 
 
+    #spliting the data
+    train_data, val_data = torch.utils.data.random_split(train_data, [2400,300])
 
+    #loading the data
+    train_data_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+    val_data_loader = torch.utils.data.DataLoader(val_data, batch_size=64, shuffle=True)
+    test_data_loader = torch.utils.data.DataLoader(test_data,  batch_size=64, shuffle=True)
 
-
-
-
-
+    print (len(train_data_loader),len(test_data_loader))
 
 # %%
